d489d69bbe75b6546a9b37216eda8344d9cd0d51
nergtastic <nergon@gmail.com>
2018-08-16 20:48:01 -0600

    Checking in initial Music is Fun project

--- itunes-controller.js
+++ itunes-controller.js
@@ -6,9 +6,31 @@ function ItunesController() {
   function drawSongs(results) {
     console.log(results)
     //YOUR CODING STARTS HERE
-
-
-
+    let template = ``
+    document.getElementById('count').innerText = "Showing " + results.length + " results for: " + document.getElementById('artist').value
+    for(let i=0; i < results.length; i++){
+      const song = results[i];
+        template += `
+        <div style="margin: 30px 0px;">
+        <tr style="background: url(${song.albumArt}); background-repeat: no-repeat; background-size: cover; background-position: center; 
+        background-color: rgba(255, 255, 255, .85); background-blend-mode: lighten; border-radius: 20px; margin: 30px 0px;">
+          <th scope="row"></th>
+          <td><img style="border-radius: 20px;" src="${song.albumArt}" alt="" style="width: 250px;"></td>
+          <td class="text-gray song-details" ;>
+            <h3 class="song-item text-truncate">Song: ${song.title}</h3>
+            <h3 class="song-item">By: ${song.artist}</h3>
+            <h3 class="song-item">From: ${song.collection}</h3>
+            <audio controls class="song-item">
+              <source src="${song.preview}">
+            </audio>
+            <br />
+            <h4 class="song-item">Own this album for: $${song.price}</h4>
+          </td>
+          </tr>
+          </div>
+      `
+    }
+    document.getElementById('songs').innerHTML = template
   }
 
   //PUBLIC
--- itunes-service.js
+++ itunes-service.js
@@ -1,8 +1,33 @@
 function ItunesService() {
 
   //DO NOT MODIFY
+//   this.getMusicByArtist = function (artist) {
+//     let url1 = '//bcw-getter.herokuapp.com/?url='
+//     let url2 = 'https://itunes.apple.com/search?term=' + artist
+//     let apiUrl = url1 + url2
+//     //Casts each object to 
+//     return $.getJSON(apiUrl).then(function (response) {
+//       console.log(response)
+//       var songList = response.results.map(song => {
+//         return {
+//           title: song.trackName,
+//           albumArt: song.artworkUrl60.replace(/60x60/g, "250x250"),
+//           artist: song.artistName,
+//           collection: song.collectionName,
+//           price: song.collectionPrice,
+//           preview: song.previewUrl,
+//         }
+//       })
+//       return songList;
+//     })
+//   }
+// }
+
+//Use this at home:
+  //DO NOT MODIFY
   this.getMusicByArtist = function (artist) {
-    var url = 'https://itunes.apple.com/search?term=' + artist + "&callback=?"
+    var url = 'https://itunes.apple.com/search?term=' + artist + "&callback=?" + "&media=music"
+    console.log(url)
     //Casts each object to 
     return $.getJSON(url).then(function (response) {
       var songList = response.results.map(song => {
